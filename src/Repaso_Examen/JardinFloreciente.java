0120011a0fd0aefe5a4d754c5bbd50736a0884a7
msceada1 <195111856+msceada1@users.noreply.github.com.>
2025-11-06 21:24:14 +0100

    respasanso para el examen

--- src/Repaso_Examen/JardinFloreciente.java
+++ src/Repaso_Examen/JardinFloreciente.java
@@ -6,7 +6,7 @@ public class JardinFloreciente {
     static final int AGUA = 2;
     static final int ABEJA = 5;
     static final int MALA_HIERBA = 3;
-    static final int GUSANO = 3;
+    static final int GUSANO = 4;
 
     public static void main(String[] args) {
 
@@ -27,30 +27,35 @@ public class JardinFloreciente {
                 {0, 1, 2, 1, 0, 0, 0, 0, 0, 2, 3, 1, 0, 0, 1},
                 {1, 3, 4, 0, 0, 5, 0, 0, 1, 0, 0, 0, 0, 4, 2}
         };
-    }
 
-    private static boolean haySemilla(int[][] matriz) {
-
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                if (matriz[i][j] == SEMILLA) {
-                    return true;
+        for (int i = 0; i < jardin.length; i++) {
+            for (int j = 0; j < jardin[i].length; j++) {
+                if (florecera(jardin, i, j)) {
+                    System.out.println("la poscion " + i + " " + j + " florecera");
                 }
             }
         }
-
-        return false;
     }
 
-    private static boolean contieneElemento(int[][] matriz, int i, int j, int elemento) {
+    private static boolean haySemilla(int[][] jardin, int i, int j) {
 
-        for (int k = i - 1; k <= matriz.length + i; k++) {
+        if (jardin[i][j] != SEMILLA) {
+            return false;
+
+        }
+
+        return true;
+    }
+
+    private static boolean contieneElemento(int[][] jardin, int i, int j, int elemento) {
+
+        for (int k = i - 1; k <= i + 1; k++) {
             for (int l = j - 1; l <= j + 1; l++) {
                 if (k == i && l == j) {
                     continue;
                 }
-                if (k >= 0 && k < matriz.length && j >= 0 && j < matriz[0].length) {
-                    if (matriz[k][j] == elemento) {
+                if (k >= 0 && k < jardin.length && l >= 0 && l < jardin[0].length) {
+                    if (jardin[k][l] == elemento) {
                         return true;
                     }
                 }
@@ -60,24 +65,20 @@ public class JardinFloreciente {
         return false;
     }
 
-    private static boolean hayAgua(int[][] matriz, int i, int j) {
+    private static boolean hayAgua(int[][] jardin, int i, int j) {
 
-        if (contieneElemento(matriz, i, j, AGUA)) {
-            return true;
-        }
-
-        return false;
+        return contieneElemento(jardin, i, j, AGUA);
     }
 
-    private static boolean hayAbeja(int[][] matriz, int i, int j) {
+    private static boolean hayAbeja(int[][] jardin, int i, int j) {
 
         for (int k = i - 3; k <= i + 3; k++) {
             for (int l = j - 3; l <= j + 3; l++) {
                 if (k == i && l == j) {
                     continue;
                 }
-                if (k >= 0 && k < matriz.length && l >= 0 && l < matriz[0].length) {
-                    if (matriz[k][l] == ABEJA) {
+                if (k >= 0 && k < jardin.length && l >= 0 && l < jardin[0].length) {
+                    if (jardin[k][l] == ABEJA) {
                         return true;
                     }
                 }
@@ -87,22 +88,18 @@ public class JardinFloreciente {
         return false;
     }
 
-    private static boolean hayMalaHierba(int[][] matriz, int i, int j, int elemento) {
+    private static boolean hayGusano(int[][] jardin, int i, int j) {
 
-        if (contieneElemento(matriz, i, j, MALA_HIERBA)) {
-            return true;
-        }
-
-        return false;
+        return contieneElemento(jardin, i, j, GUSANO);
     }
 
-    private static boolean hayGusano(int[][] matriz, int i, int j) {
+    private static boolean hayMalaHierba(int[][] jardin, int i, int j) {
 
-        if (contieneElemento(matriz, i, j, GUSANO)) {
-            return true;
-        }
-
-        return false;
+        return contieneElemento(jardin, i, j, MALA_HIERBA);
     }
 
+    private static boolean florecera(int[][] jardin, int i, int j) {
+
+        return haySemilla(jardin, i, j) && hayAgua(jardin, i, j) && hayAbeja(jardin, i, j) && (!hayMalaHierba(jardin, i, j) || hayMalaHierba(jardin, i, j) && hayGusano(jardin, i, j));
+    }
 }
