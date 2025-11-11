30c77f67f252a9b4a79bdb897b373f2ea1f9e3d7
msceada1 <195111856+msceada1@users.noreply.github.com.>
2025-11-11 19:24:26 +0100

    ejercicio 2020

--- src/Mis_Ejercicios/Ejercicio1Examen2020.java
+++ src/Mis_Ejercicios/Ejercicio1Examen2020.java
@@ -9,21 +9,61 @@ public class Ejercicio1Examen2020 {
         int[] votosPartidoRepublicano = {1318255, 163387, 1252401, 684872, 4483810, 1202484, 673215, 185127, 12723, 4617886, 2089104, 128847, 409055, 2146015, 1557286, 800983, 671018, 1202971, 1178638, 671154, 943169, 1090893, 2279543, 1322951, 700714, 1594511, 279240, 991922, 512058, 345790, 1601933, 319667, 2819534, 2362631, 216794, 2841005, 949136, 782403, 2970733, 180543, 1155389, 227721, 1522925, 4685047, 515231, 95369, 1769443, 1221747, 489371, 1405284, 174419};
         int[] votosPartidoDemocrata = {729547, 116454, 1161167, 380494, 8753788, 1338870, 897572, 235603, 282830, 4504975, 1877963, 266891, 189765, 3090729, 1033126, 653669, 427005, 628854, 780154, 715326, 1677928, 1995196, 2268839, 1367716, 485131, 1071068, 177709, 568988, 539260, 348526, 2148278, 385234, 4556124, 2189316, 93758, 2394164, 420375, 1002106, 2926441, 252525, 855373, 117458, 870695, 3877868, 310676, 178573, 1981473, 1742718, 188794, 1382536, 55973};
 
+        if (esDelPartidoRepublicano(votosPartidoRepublicano, votosPartidoDemocrata, electores)) {
+            System.out.println("El presidente es del partido republicano");
+        } else {
+            System.out.println("el presidente es del partido democrata");
+        }
+
+        //  int[] electoresGanadosPorPartido = electoresGanadosPorPartido(votosPartidoRepublicano, votosPartidoDemocrata, electores);
+
+        System.out.println("los votos del partido republicano son " + votosTotalesPartidoRepublicano(votosPartidoRepublicano));
+        System.out.println("Los votos del partido democrata son " + votosTotalesPartidoDemocrata(votosPartidoDemocrata));
+
     }
 
-    private static int[] votosPorPartido(int[] votosPartdoRepublicano, int[] votosPartidoDemocrata, int[] electores) {
+    private static int[] electoresGanadosPorPartido(int[] votosPartdoRepublicano, int[] votosPartidoDemocrata, int[] electores) {
 
         int[] votosPorPartido = new int[2];
 
         for (int i = 0; i < votosPartdoRepublicano.length; i++) {
             if (votosPartdoRepublicano[i] > votosPartidoDemocrata[i]) {
-                votosPorPartido[0] += electores[i];
+                votosPorPartido[0] += electores[i]; //republicanos
             } else {
-                votosPorPartido[1] += electores[i];
+                votosPorPartido[1] += electores[i]; //democrata
             }
         }
 
         return votosPorPartido;
+    }
+
+    private static boolean esDelPartidoRepublicano(int[] votosPartidoRepublicano, int[] votosPartidoDemocrata, int[] electores) {
+
+        int[] votosPorPartido = electoresGanadosPorPartido(votosPartidoRepublicano, votosPartidoDemocrata, electores);
+
+        return votosPorPartido[0] > votosPorPartido[1];
+    }
+
+    private static int votosTotalesPartidoRepublicano(int[] votosRepublicanos) {
+
+        int sumaVotos = 0;
+
+        for (int i = 0; i < votosRepublicanos.length; i++) {
+            sumaVotos += votosRepublicanos[i];
+        }
+
+        return sumaVotos;
+    }
+
+    private static int votosTotalesPartidoDemocrata(int[] votosPartidoDemocrata) {
+
+        int sumaVotos = 0;
+
+        for (int i = 0; i < votosPartidoDemocrata.length; i++) {
+            sumaVotos += votosPartidoDemocrata[i];
+        }
+
+        return sumaVotos;
     }
 
 }
